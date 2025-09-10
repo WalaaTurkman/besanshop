997aec885283ea644cc8138bea2ac5a7ddaf2442
WalaaTurkman <s12323751@stu.najah.edu>
2025-09-11 00:51:43 +0300

    3ed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,41 @@
 // src/pages/Home.js
-import { Container, Row, Col } from "react-bootstrap";
+
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import BasicExample from "../componnt/Card";
 import UncontrolledExample from "../componnt/Carousel";
 import "../App.css";
 
-// مصفوفة المنتجات
+// ✅ لازم يكون تعريف المنتجات هنا خارج الـ function
 const products = [
   {
-    title: "كوب 1",
-    text: "وصف الكوب",
+    title: "مچ بطوطة بُني",
+    text: "20 شيكل",
     image: "/images/cups/batamug.jpg",
   },
   {
-    title: "البرمجة",
-    text: "أساس تطوير التطبيقات والمواقع.",
+    title: "فنجان هاند ميد الغيوم",
+    text: "30 شيكل",
     image: "/images/cups/cloudmug.jpg",
   },
   {
-    title: "التعلم العميق",
-    text: "فرع متقدم من الذكاء الاصطناعي.",
+    title: "مچ هاند ميد مورد بدون صحن",
+    text: "25 شيكل",
     image: "/images/cups/flowermug.jpg",
+  },
+  { title: "Miu miu",
+    text: "150 شيكل",
+    image: "/images/bags/Miumiu.jpg",
+  },
+  {
+    title: "Lacoste",
+    text: "100 شيكل",
+    image: "/images/bags/Lacoste (2).jpg",
+  },
+  {
+    title: "Chloè",
+    text: "130 شيكل",
+    image: "/images/bags/Chloè.jpg",
   },
 ];
 
@@ -38,7 +54,25 @@ function Home() {
           ⭐ التشكيلة المميزة
         </h2>
 
-        <Row>
+        {/* الأزرار */}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "Flex-Start",
+            marginTop: "2rem",
+            gap: "1rem",
+          }}
+        >
+          <Link to="/bags">
+            <Button variant="dark">👜 عرض المزيد - الحقائب</Button>
+          </Link>
+          <Link to="/cups">
+            <Button variant="dark">☕ عرض المزيد - الأكواب</Button>
+          </Link>
+        </div>
+
+        {/* الكروت */}
+        <Row style={{ direction: "rtl", marginTop: "2rem" }}>
           {products.map((item, index) => (
             <Col key={index} md={4}>
               <BasicExample
